49b6a93a66cfb6aac6d8eeffd2eef1ce47241f94
EJ-David <daviderhabor.de@gmail.com>
2022-09-15 03:22:30 +0100

    basic setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Beautiful Counter for Practicing Hook Funtionalities.
+    </div>
+  );
+}
+
+export default App;
