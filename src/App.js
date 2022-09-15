90cb132b7507d7ef99a2909888ec272e0d1d83a4
EJ-David <daviderhabor.de@gmail.com>
2022-09-16 00:11:57 +0100

    Design Complete, CSS done.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import "./App.css";
+// import down from '../src/icons/down.png' use icons 
 
 function App() {
   return (
     <div className="App">
       <div className="counter1">
         <div className="display"></div>
-        <button className="incr"></button>
-        <button className="decr"></button>
+        <button className="incr">+</button>
+        <button className="decr">-</button>
       </div>
       <div className="counter2">
         <div className="display"></div>
-        <button className="incr"></button>
-        <button className="decr"></button>
+        <button className="incr">+</button>
+        <button className="decr">-</button>
       </div>
     </div>
   );
