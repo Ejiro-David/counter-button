29af1111341390ebec0db251dbd7cc27d487f359
EJ-David <daviderhabor.de@gmail.com>
2022-09-16 00:41:49 +0100

    working on state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
+import { useState } from "react";
 import "./App.css";
 // import down from '../src/icons/down.png' use icons 
 
+const Counter1 = ({display}) => {
+  return (
+    <div className="counter1">
+        <div className="display">{display}</div>
+        <button className="incr">+</button>
+        <button className="decr">-</button>
+      </div>
+  )
+}
+
+const Counter2 = ({display}) => {
+  return (
+    <div className="counter2">
+        <div className="display">{display}</div>
+        <button className="incr">+</button>
+        <button className="decr">-</button>
+      </div>
+  )
+}
+
 function App() {
+  const [firstDisplay, setFirstDisplay] = useState(0);
+  const [secondDisplay, setSecondDisplay] = useState(0);
+  
   return (
     <div className="App">
-      <div className="counter1">
-        <div className="display"></div>
-        <button className="incr">+</button>
-        <button className="decr">-</button>
-      </div>
-      <div className="counter2">
-        <div className="display"></div>
-        <button className="incr">+</button>
-        <button className="decr">-</button>
-      </div>
+      <Counter1 display={firstDisplay}/>
+      <Counter2 display={secondDisplay}/>
     </div>
   );
 }
